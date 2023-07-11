b12635b000d1ab2f5b7bbd0f8b47a75e8aaf2fd7
Angela Ung <angela.ung@ukg.com>
2023-07-11 13:36:19 -0400

    configure landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,29 +10,24 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
+      <h1 className="text-3xl font-bold">
+        48 Hours Project: Spirit
+      </h1>
       <ul>
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+            href="/manager"
           >
-            15m Quickstart Blog Tutorial
+            Manager View
           </a>
         </li>
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
+            href="/employee"
           >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            Employee View
           </a>
         </li>
       </ul>
