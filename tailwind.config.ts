def106aee38fd9830dc76f5812afa982f7cf7d96
Angela Ung <angela.ung@ukg.com>
2023-07-11 16:41:59 -0400

    change status

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,18 @@ import type { Config } from 'tailwindcss'
 export default {
 	content: ['./app/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'ukg-light-teal': '#30CEBB',
+				'ukg-dark-teal': "#005151",
+				'ukg-gray': "#53565A",
+				'ukg-yellow': "ffc600",
+				'ukg-light-gray': "#F5F5F5",
+				'ukg-green': "786d4b",
+				'ukg-pink': "#C724B1",
+				'ukg-purple': "#59178A"
+			},
+		},
 	},
 	plugins: [],
 } satisfies Config
