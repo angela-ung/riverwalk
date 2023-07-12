bbc97ee0909b68ec46b25885babdacf2466ba477
Angela Ung <angela.ung@ukg.com>
2023-07-12 15:50:21 -0400

    install charts

--- app/routes/analysis._index.tsx
+++ app/routes/analysis._index.tsx
@@ -1,28 +1,103 @@
-import type { V2_MetaFunction } from "@remix-run/node";
-import React from 'react';
+import { json, V2_MetaFunction } from '@remix-run/node';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js/auto';
+import { Line } from 'react-chartjs-2';
+import { faker } from '@faker-js/faker';
+import { ClientOnly } from 'remix-utils';
+import { useLoaderData } from '@remix-run/react/dist/components';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "48 Hours Project" },
-    { name: "description", content: "Made by Angela Ung, Neyaa " },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
+export async function loader() {
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+  ];
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Team Emotion',
+      },
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Focused',
+        data: labels.map(() => faker.number.int({ min: 0, max: 100 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Relaxed',
+        data: labels.map(() => faker.number.int({ min: 0, max: 100 })),
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+  return json({ options, data });
+}
+
 export default function AnalysisIndex() {
+  const { options, data } = useLoaderData<typeof loader>();
   return (
-		<div className="lg:text-center py-16 bg-gray-100">
-						<h2 className="text-base mt-2 mb-6 text-3xl sm:text-4xl font-bold tracking-tight leading-7 text-teal-900">
-							UKG Dashboard
-						</h2>
-            <ul className="mx-auto max-w-2xl py-4 divide-y divide-gray-100 rounded-md border border-gray-200 bg-white">
-              THIS IS WHERE YOU PUT THE GRAPH
-                <div className="text-right">
-                <a className="ml-60 flex-shrink-0 text-right text-sm m-6 p-6 font-medium text-teal-900 hover:text-teal-800"
-                    href="/">
-                      Back
-                  </a>
-                </div>
-            </ul>
-		</div>
+    <div className="lg:text-center py-16 bg-gray-100">
+      <h2 className="text-base mt-2 mb-6 text-3xl sm:text-4xl font-bold tracking-tight leading-7 text-teal-900">
+				UKG Dashboard
+			</h2>
+      <ul className="mx-auto max-w-2xl py-4 rounded-md border border-gray-200 bg-white">
+        <div className="p-8">
+          <ClientOnly fallback={<Fallback />}>
+            {() => <Line options={options} data={data} />}
+          </ClientOnly>
+        </div>
+        <div className="text-right">
+          <a className="ml-60 flex-shrink-0 text-right text-sm m-6 p-6 font-medium text-teal-900 hover:text-teal-800"
+              href="/">
+              Back
+          </a>
+        </div>
+      </ul>
+    </div>
   );
+}
+
+function Fallback() {
+  return <div>Generating Chart</div>;
 }
